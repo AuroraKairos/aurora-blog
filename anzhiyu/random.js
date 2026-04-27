001ef2031088baf1dc05d48eea60c7931f24db5d
yangchao <1453504016@qq.com>
2026-04-27 23:04:13 +0800

    Site updated: 2026-04-27 23:04:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/04/26/hello-world/"];function toRandomPost(){
+var posts=["2026/04/26/hello-world/","2026/04/27/Java工程师AI大模型学习计划/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
